47d30451fa29484a3c7ca74f5aa4e79fd4fddb8c
mistervenik <mistercoder1@gmail.com>
2020-11-25 21:36:57 +0300

    Исправление ошибок импортируемых библиотек в проект

--- client/src/main/java/com/client/login/LoginController.java
+++ client/src/main/java/com/client/login/LoginController.java
@@ -8,7 +8,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
--- server/src/main/java/com/server/Server.java
+++ server/src/main/java/com/server/Server.java
@@ -1,4 +1,201 @@
 package com.server;
 
+import com.messages.Message;
+import com.messages.MessageType;
+import com.messages.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class Server {
+
+    private static final int PORT = 9001;
+    private static final HashMap<String, User> names = new HashMap<>();
+    private static HashSet<ObjectOutputStream> writers = new HashSet<>();
+    private static ArrayList<User> users = new ArrayList<>();
+    static Logger logger = LoggerFactory.getLogger(Server.class);
+
+    public static void main(String[] args) {
+        logger.info("Сервер успешно запущен!");
+
+        try (ServerSocket listener = new ServerSocket(PORT)) {
+            while (true) {
+                new Handler(listener.accept()).start();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private static class Handler extends Thread {
+        private String name;
+        private Socket socket;
+        private Logger logger = LoggerFactory.getLogger(Handler.class);
+        private User user;
+        private ObjectInputStream objectInputStream;
+        private OutputStream outputStream;
+        private ObjectOutputStream output;
+        private InputStream inputStream;
+
+        public Handler(Socket socket) throws IOException {
+            this.socket = socket;
+        }
+
+        public void run() {
+            logger.info("Попытка подключить пользователя...");
+            try {
+                inputStream = socket.getInputStream();
+                objectInputStream = new ObjectInputStream(inputStream);
+                outputStream = socket.getOutputStream();
+                output = new ObjectOutputStream(outputStream);
+
+                Message firstMessage = (Message) objectInputStream.readObject();
+                checkUsername(firstMessage);
+                writers.add(output);
+//                sendNotification(firstMessage);
+                addToList();
+
+                while (socket.isConnected()) {
+                    Message inputmsg = (Message) objectInputStream.readObject();
+                    if (inputmsg != null) {
+                        logger.info(inputmsg.getType() + " - " + inputmsg.getName() + ": " + inputmsg.getMsg());
+                        switch (inputmsg.getType()) {
+                            case USER:
+                            case VOICE:
+                                write(inputmsg);
+                                break;
+                            case CONNECTED:
+                                addToList();
+                                break;
+                        }
+                    }
+                }
+            } catch (SocketException socketException) {
+                logger.error("Исключение сокета для пользователя " + name);
+            } catch (Exception e){
+                logger.error("Исключение в методе run() для пользователя: " + name, e);
+            } finally {
+                closeConnections();
+            }
+        }
+
+        private synchronized void checkUsername(Message firstMessage) {
+            logger.info(firstMessage.getName() + " пытается подключиться");
+            if (!names.containsKey(firstMessage.getName())) {
+                this.name = firstMessage.getName();
+                user = new User();
+                user.setName(firstMessage.getName());
+                user.setPicture(firstMessage.getPicture());
+
+                users.add(user);
+                names.put(name, user);
+
+                logger.info(name + " добавлен в список");
+            }
+        }
+
+//        private Message sendNotification(Message firstMessage) throws IOException {
+//            Message msg = new Message();
+//            msg.setMsg("присоединился к чату.");
+//            msg.setType(MessageType.NOTIFICATION);
+//            msg.setName(firstMessage.getName());
+//            msg.setPicture(firstMessage.getPicture());
+//            write(msg);
+//            return msg;
+//        }
+
+
+        private Message removeFromList() throws IOException {
+            logger.debug("removeFromList() method Enter");
+            Message msg = new Message();
+            msg.setMsg("покинул чат.");
+            msg.setType(MessageType.DISCONNECTED);
+            msg.setName("SERVER");
+            msg.setUserlist(names);
+            write(msg);
+            logger.debug("removeFromList() method Exit");
+            return msg;
+        }
+
+        /*
+         * Для отображения того, что пользователь присоединился к серверу
+         */
+        private Message addToList() throws IOException {
+            Message msg = new Message();
+            msg.setMsg("Хорошего ЧСВ общения!");
+            msg.setType(MessageType.CONNECTED);
+            msg.setName("SERVER");
+            write(msg);
+            return msg;
+        }
+
+        /*
+         * Создает и отправляет слушателям тип сообщения.
+         */
+        private void write(Message msg) throws IOException {
+            for (ObjectOutputStream writer : writers) {
+                msg.setUserlist(names);
+                msg.setUsers(users);
+                msg.setOnlineCount(names.size());
+                writer.writeObject(msg);
+                writer.reset();
+            }
+        }
+
+        /*
+         * Как только пользователь был отключен, мы закрываем открытые соединения и удаляем писателей.
+         */
+        private synchronized void closeConnections()  {
+            logger.debug("closeConnections() method Enter");
+            logger.info("HashMap names:" + names.size() + " writers:" + writers.size() + " usersList size:" + users.size());
+            if (name != null) {
+                names.remove(name);
+                logger.info("Пользователь: " + name + " был удален!");
+            }
+            if (user != null){
+                users.remove(user);
+                logger.info("Пользовательский объект: " + user + " был удален!");
+            }
+            if (output != null){
+                writers.remove(output);
+                logger.info("Writer object: " + user + " был удален!");
+            }
+            if (inputStream != null){
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (outputStream != null){
+                try {
+                    outputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (objectInputStream != null){
+                try {
+                    objectInputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            try {
+                removeFromList();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            logger.info("HashMap names:" + names.size() + " writers:" + writers.size() + " usersList size:" + users.size());
+            logger.debug("closeConnections() method Exit");
+        }
+    }
 }
